05eb72be73b47ef3f1eb19e53c151e3e75b26d98
Matt Inkeles <mattinkeles37@gmail.com>
2022-02-01 23:39:11 -0500

    -Server header wip

--- portfolio/src/components/channel-panel/channel-panel.tsx
+++ portfolio/src/components/channel-panel/channel-panel.tsx
@@ -4,14 +4,21 @@ import './styles/channel-panel.css';
 const ChannelPanel = () => {
   return (
     <Grid container direction="column" className='ChannelPanelContainer'>
-      <Grid item className='ChannelsContainer'>
-        <Grid item>
+      <Grid item className='ServerHeaderContainer'>
+        <Grid item className='ServerHeader'>
+          <Typography className='ServerHeaderLabel'>
+            My Server
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container direction="column" className='ChannelsContainer'>
+        <Grid item xs={4}>
           <Typography>Channel 1</Typography>
         </Grid>
-        <Grid item>
+        <Grid item xs={4}>
           <Typography>Channel 2</Typography>
         </Grid>
-        <Grid item>
+        <Grid item xs={4}>
           <Typography>Channel 3</Typography>
         </Grid>
       </Grid>
