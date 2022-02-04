153bb5e2654dc003b53cf1b9bfb0eaf078e2d755
Matt Inkeles <mattinkeles37@gmail.com>
2022-02-03 20:38:14 -0500

    -more styling for the header

--- portfolio/src/components/channel-panel/channel-panel.tsx
+++ portfolio/src/components/channel-panel/channel-panel.tsx
@@ -1,14 +1,20 @@
 import { Grid, Typography } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './styles/channel-panel.css';
 
 const ChannelPanel = () => {
   return (
     <Grid container direction="column" className='ChannelPanelContainer'>
       <Grid item className='ServerHeaderContainer'>
-        <Grid item className='ServerHeader'>
-          <Typography className='ServerHeaderLabel'>
-            My Server
-          </Typography>
+        <Grid container direction='row' className='ServerHeader'>
+          <Grid item width='85%'>
+            <Typography className='ServerHeaderLabel'>
+              My Server
+            </Typography>
+          </Grid>
+          <Grid item>
+            <ExpandMoreIcon className='Chevron' />
+          </Grid>
         </Grid>
       </Grid>
       <Grid container direction="column" className='ChannelsContainer'>
