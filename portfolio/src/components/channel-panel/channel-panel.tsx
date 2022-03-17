f33fa8fe312bbccc90dd731fd7f2e2d0ca26c083
Matt Inkeles <mattinkeles37@gmail.com>
2022-03-16 23:27:51 -0400

    -Populated user panel

--- portfolio/src/components/channel-panel/channel-panel.tsx
+++ portfolio/src/components/channel-panel/channel-panel.tsx
@@ -1,7 +1,18 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, styled, Typography, withStyles } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './styles/channel-panel.css';
 import { ReactComponent as WavyDude } from '../../common/images/wavyDude.svg';
+import { ReactComponent as Projects } from '../../common/images/projects.svg';
+import { ReactComponent as Experience } from '../../common/images/experience.svg';
+import MattProfile from '../../common/images/mattProfile.jpg';
+
+const GrayTypography = styled(Typography)({
+  color: 'rgb(142, 146, 151)',
+});
+
+const WhiteTypography = styled(Typography)({
+  color: 'white',
+});
 
 const ChannelPanel = () => {
   return (
@@ -10,9 +21,9 @@ const ChannelPanel = () => {
         <Grid item className='ServerHeaderContainer'>
           <Grid container direction='row' className='ServerHeader'>
             <Grid item width='85%'>
-              <Typography className='ServerHeaderLabel'>
-                Matt Inkeles
-              </Typography>
+              <WhiteTypography className='ServerHeaderLabel'>
+                My Resume
+              </WhiteTypography>
             </Grid>
             <Grid item>
               <ExpandMoreIcon className='Chevron' />
@@ -21,22 +32,31 @@ const ChannelPanel = () => {
         </Grid>
         <Grid container direction='column' className='ChannelsContainer'>
           <Grid item sx={{ height: '33%' }}>
-            <Grid container direction='row'>
-              <WavyDude />
-              <Typography>About Me</Typography>
+            <Grid container direction='row' className='ChannelItem'>
+              <WavyDude className='ChannelIcon'/>
+              <GrayTypography>About Me</GrayTypography>
             </Grid>
           </Grid>
           <Grid item sx={{ height: '33%' }}>
-            <Typography>Projects</Typography>
+            <Grid container direction='row' className='ChannelItem'>
+              <Projects className='ChannelIcon'/>
+              <GrayTypography>Projects</GrayTypography>
+            </Grid>
           </Grid>
           <Grid item sx={{ height: '33%' }}>
-            <Typography>Contact</Typography>
+            <Grid container direction='row' className='ChannelItem'>
+              <Experience className='ChannelIcon'/>
+              <GrayTypography>Experience</GrayTypography>
+            </Grid>
           </Grid>
           <Grid item sx={{height: ''}}></Grid>
         </Grid>
       </Grid>
       <Grid item className='UserPanel'>
-      
+        <Grid container direction='row' className='ChannelItem'>
+          <img  src={MattProfile} className='ProfilePic' />
+          <WhiteTypography>Matt Inkeles</WhiteTypography>
+        </Grid>
       </Grid>
     </Grid>
   );
