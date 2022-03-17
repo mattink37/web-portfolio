d6ac9f3382944f676a0f939b28771c9585efa6bd
Matt Inkeles <mattinkeles37@gmail.com>
2022-03-16 22:12:21 -0400

    -aligned user panel to bottom of page

--- portfolio/src/components/channel-panel/channel-panel.tsx
+++ portfolio/src/components/channel-panel/channel-panel.tsx
@@ -1,37 +1,42 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './styles/channel-panel.css';
+import { ReactComponent as WavyDude } from '../../common/images/wavyDude.svg';
 
 const ChannelPanel = () => {
   return (
-    <Grid container direction="column" className='ChannelPanelContainer'>
-      <Grid item className='ServerHeaderContainer'>
-        <Grid container direction='row' className='ServerHeader'>
-          <Grid item width='85%'>
-            <Typography className='ServerHeaderLabel'>
-              My Server
-            </Typography>
+    <Grid container direction='column' className='ChannelPanelContainer'>
+      <Grid container direction='column' sx={{ flex: 1 }}>
+        <Grid item className='ServerHeaderContainer'>
+          <Grid container direction='row' className='ServerHeader'>
+            <Grid item width='85%'>
+              <Typography className='ServerHeaderLabel'>
+                Matt Inkeles
+              </Typography>
+            </Grid>
+            <Grid item>
+              <ExpandMoreIcon className='Chevron' />
+            </Grid>
           </Grid>
-          <Grid item>
-            <ExpandMoreIcon className='Chevron' />
+        </Grid>
+        <Grid container direction='column' className='ChannelsContainer'>
+          <Grid item sx={{ height: '33%' }}>
+            <Grid container direction='row'>
+              <WavyDude />
+              <Typography>About Me</Typography>
+            </Grid>
           </Grid>
+          <Grid item sx={{ height: '33%' }}>
+            <Typography>Projects</Typography>
+          </Grid>
+          <Grid item sx={{ height: '33%' }}>
+            <Typography>Contact</Typography>
+          </Grid>
+          <Grid item sx={{height: ''}}></Grid>
         </Grid>
       </Grid>
-      <Grid container direction="column" className='ChannelsContainer'>
-        <Grid item xs={4}>
-          <Typography>Channel 1</Typography>
-        </Grid>
-        <Grid item xs={4}>
-          <Typography>Channel 2</Typography>
-        </Grid>
-        <Grid item xs={4}>
-          <Typography>Channel 3</Typography>
-        </Grid>
-      </Grid>
-      <Grid item className='UserPanelContainer'>
-        <Grid item className='UserPanel'>
-          
-        </Grid>
+      <Grid item className='UserPanel'>
+      
       </Grid>
     </Grid>
   );
