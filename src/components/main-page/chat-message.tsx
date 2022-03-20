4193e685a4a2fd4705f54fee75a70e1d43cb5935
Matt Inkeles <mattinkeles37@gmail.com>
2022-03-20 18:21:24 -0400

    Started filling out chat messages

---
+++ src/components/main-page/chat-message.tsx
@@ -0,0 +1,43 @@
+import { Grid } from "@mui/material";
+import moment from "moment";
+import { DateTypography, WhiteTypography } from "../../common/TypographyStyles";
+import MattProfile from "../../common/images/mattProfile.jpg";
+
+const ChatMessage: React.FC<ChatMessageProps> = ({
+  username,
+  message,
+  showAvatar,
+  showUsername,
+  showDate,
+}) => {
+  return (
+    <Grid container direction='column'>
+      <Grid container direction='row' className='MessageContainer'>
+        <Grid item className='Avatar'>
+          {showAvatar ? <img src={MattProfile} className="ProfilePicMain" /> : <div style={{ width: '40px', height: '40px' }}></div>}
+        </Grid>
+        <Grid item className='Username'>
+          {showUsername && <Grid item>
+            <WhiteTypography>{username}</WhiteTypography>
+          </Grid>}
+          {showDate && <Grid item>
+            <DateTypography>Today at {moment().format('h:mm a')}</DateTypography>
+          </Grid>}
+          <WhiteTypography>
+            {message}
+          </WhiteTypography>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+};
+
+interface ChatMessageProps {
+  username?: string;
+  message?: string;
+  showDate?: boolean;
+  showAvatar?: boolean;
+  showUsername?: boolean;
+}
+
+export default ChatMessage;
--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -1,43 +1,39 @@
 import { Grid } from "@mui/material";
-import moment from "moment";
 import { useState } from "react";
-import { DateTypography, WhiteTypography } from "../../common/TypographyStyles";
 import ChannelPanel from "../channel-panel/channel-panel";
 import TopBar from "../page-header/topbar";
 import ServerSidebar from "../server-sidebar/server-side-bar";
-import './styles/main-page.css';
-import MattProfile from "../../common/images/mattProfile.jpg";
+import "./styles/main-page.css";
 import { RemoveScrollBar } from "react-remove-scroll-bar";
+import ChatMessage from "./chat-message";
 
 const MainPage = () => {
-  const [headerText, setHeaderText] = useState('About Me');
+  const [headerText, setHeaderText] = useState("About Me");
   return (
-    <Grid container className='MainPageContainer'>
+    <Grid container className="MainPageContainer">
       <RemoveScrollBar />
       <Grid item>
         <ServerSidebar />
       </Grid>
       <Grid item>
-        <ChannelPanel setHeaderText={setHeaderText}/>
+        <ChannelPanel setHeaderText={setHeaderText} />
       </Grid>
-      <Grid item sx={{ flex: '1' }}>
-        <TopBar headerText={headerText}/>
-        <Grid container direction='column'>
-          <Grid container direction='row' className='MessageContainer'>
-            <Grid item className='Avatar'>
-              <img src={MattProfile} className="ProfilePicMain" />
-            </Grid>
-            <Grid item className='Username'>
-              <Grid item>
-                <WhiteTypography>Matt Inkeles</WhiteTypography>
-              </Grid>
-              <Grid item>
-                <DateTypography>Today at {moment().format('h:mm a')}</DateTypography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>  
+      <Grid item sx={{ flex: "1" }}>
+        <TopBar headerText={headerText} />
+        <ChatMessage
+          username="Matt Inkeles"
+          message="Hi! I'm Matt. I'm a software engineer from Atlanta, Georgia and I love creating projects like the one you're looking at right now! Most of the components here are interactive so feel free to check them out."
+          showAvatar
+          showUsername
+          showDate
+        />
+        <ChatMessage
+          showUsername={false}
+          showDate={false}
+          showAvatar={false}
+          message="I grew up playing all sorts of video games and that sparked my interest in computer science."
+        />
+      </Grid>
     </Grid>
   );
 };
