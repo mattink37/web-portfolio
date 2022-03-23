fba789f581a5f467f1a517dee7c04df4186cd053
Matt Inkeles <mattinkeles37@gmail.com>
2022-03-23 18:10:10 -0400

    Updated sidebar images

--- src/components/server-sidebar/server-side-bar.tsx
+++ src/components/server-sidebar/server-side-bar.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import { HslaColorObject } from "../../common/colors";
 import ServerBubble from "../server-bubble/server-bubble";
 import Divider from "./divider";
@@ -15,18 +15,18 @@ const ServerSidebar = () => {
     <Grid container direction="column" className='SidebarContainer'>
       <Grid item className='TopPadding'>
         <Grid item className='HomeBubble'>
-          <ServerBubble imgSrc="./profile-pic" onClick={() => console.log('clicked!')} />
+          <ServerBubble imgSrc="./discord.jpg" />
         </Grid>
         <Divider width={32} height={2} hsla={hsla} />
         <Grid item className='ServerContainer'>
           <Grid item className="ServerBubblePadding">
-            <ServerBubble imgSrc="./profile-pic" onClick={() => console.log('clicked!')} />
+            <ServerBubble imgSrc="./code.jpg" />
           </Grid>
           <Grid item className="ServerBubblePadding">
-            <ServerBubble imgSrc="./profile-pic" />
+            <ServerBubble imgSrc="./hockey.jpg" />
           </Grid>
           <Grid item className="ServerBubblePadding">
-            <ServerBubble imgSrc="./profile-pic" />
+            <ServerBubble imgSrc="./vidya.jpg" />
           </Grid>
         </Grid>
       </Grid>
