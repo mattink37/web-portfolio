890de8f0f213a7803d7911bb7c246be5bef08734
Matt Inkeles <mattinkeles37@gmail.com>
2025-05-16 18:22:15 -0400

    support images

--- src/components/main-page/channels/AboutMe.tsx
+++ src/components/main-page/channels/AboutMe.tsx
@@ -1,18 +1,42 @@
 import ChatMessage from '../chat-message';
+import someImage from '../../../common/images/ksu_statue.jpg'; // Replace with the actual path to your image
+import { Grid } from '@mui/material';
 
 const AboutMeChannel = (): JSX.Element => {
   return (
-    <>
+    <Grid container>
       <ChatMessage
         username="Matt Inkeles"
-        message="Hi! I'm Matt. I'm a software engineer from Atlanta, Georgia and I love creating projects like the one you're looking at right now! I grew up playing all sorts of video games and that sparked my interest in computer science. So now, all these later I've pursued my dreams and I've made a career of my passion. I graduated from Kennesaw State University and I currently work as a frontend developer at NCR. I’m developing a next-generation point-of-sale software solution for retail stores. I’m proud of the work I do and I’m always looking to improve my code, educate myself, and become a better person in the process."
+        message="Hi! I'm Matt. I'm a software engineer from Atlanta, Georgia and I love creating projects like the one you're looking at right now!"
         showAvatar
         showUsername
         showDate
       />
       <ChatMessage
-        username='Matt Inkeles'
-        message='Whew, that got real formal, real fast. Anyways, I hope you like what you see here, so feel free to click around and learn more about me and my goals.'
+        username="Matt Inkeles"
+        message="I grew up playing all sorts of video games and that sparked my interest in computer science. So now, all these years later, I've pursued my dreams and made a career of my passion."
+        showAvatar={false}
+        showUsername={false}
+        showDate={false}
+      />
+      <ChatMessage
+        username="Matt Inkeles"
+        message="I graduated from Kennesaw State University and I currently work as a software engineer at NCR Voyix. I'm developing a next-generation point-of-sale software solution for retail stores."
+        showAvatar={false}
+        showUsername={false}
+        showDate={false}
+      />
+      {/* <ChatMessage imgSrc={someImage} /> */}
+      <ChatMessage
+        username="Matt Inkeles"
+        message="I'm proud of the work I do and I'm always looking to improve my code, educate myself, and become a better person in the process."
+        showAvatar={false}
+        showUsername={false}
+        showDate={false}
+      />
+      <ChatMessage
+        username="Matt Inkeles"
+        message="Whew, that got real formal, real fast. Anyways, I hope you like what you see here, so feel free to click around and learn more about me and my goals."
         showAvatar
         showUsername
         showDate
@@ -21,9 +45,9 @@ const AboutMeChannel = (): JSX.Element => {
         showAvatar={false}
         showDate={false}
         showUsername={false}
-        message='Enjoy!'
+        message="Enjoy!"
       />
-    </>
+    </Grid>
   );
 };
 
--- src/components/main-page/chat-message.tsx
+++ src/components/main-page/chat-message.tsx
@@ -1,41 +1,52 @@
-import { Grid } from "@mui/material";
-import moment from "moment";
-import { DateTypography, WhiteTypography } from "../../common/TypographyStyles";
-import MattProfile from "../../common/images/mattProfile.jpg";
+import { Grid } from '@mui/material';
+import moment from 'moment';
+import { DateTypography, WhiteTypography } from '../../common/TypographyStyles';
+import MattProfile from '../../common/images/mattProfile.jpg';
 
 const ChatMessage: React.FC<ChatMessageProps> = ({
   username,
   message,
+  imgSrc,
   showAvatar,
   showUsername,
   showDate,
   customDate,
 }) => {
   return (
-    <Grid container direction='column' sx={{ flexFlow: 'nowrap' }}>
-      <Grid container direction='row' className='MessageContainer'>
-        <Grid item className='Avatar'>
-          {showAvatar ? <img src={MattProfile} className="ProfilePicMain" /> : <div style={{ width: '40px' }}></div>}
+    <Grid container direction="column" sx={{ flexFlow: 'nowrap' }}>
+      <Grid container direction="row" className="MessageContainer">
+        <Grid item className="Avatar">
+          {showAvatar ? (
+            <img src={MattProfile} className="ProfilePicMain" alt="" />
+          ) : (
+            <div style={{ width: '40px' }}></div>
+          )}
         </Grid>
-        <Grid item className='Username'>
-          {showUsername && <Grid item>
-            <WhiteTypography>{username}</WhiteTypography>
-          </Grid>}
-          {showDate && <Grid item>
-            <DateTypography>{customDate ?? `Today at ${moment().format('h:mm a')}`}</DateTypography>
-          </Grid>}
-          <WhiteTypography>
-            {message}
-          </WhiteTypography>
+        <Grid item className="Username">
+          {showUsername && (
+            <Grid item>
+              <WhiteTypography>{username}</WhiteTypography>
+            </Grid>
+          )}
+          {showDate && (
+            <Grid item>
+              <DateTypography>
+                {customDate ?? `Today at ${moment().format('h:mm a')}`}
+              </DateTypography>
+            </Grid>
+          )}
+          {message && <WhiteTypography>{message}</WhiteTypography>}
+          {imgSrc && <img src={imgSrc} alt="" style={{ maxWidth: '100%' }} />}
         </Grid>
       </Grid>
     </Grid>
-  )
+  );
 };
 
 interface ChatMessageProps {
   username?: string;
   message?: string;
+  imgSrc?: string;
   showDate?: boolean;
   customDate?: string;
   showAvatar?: boolean;
